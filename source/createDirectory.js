36e851be50f8282b3bef37e2f1a804eaae208cca
Perry Mitchell <perry@perrymitchell.net>
2021-11-22 21:16:47 +0200

    Fix directory creation

--- source/createDirectory.js
+++ source/createDirectory.js
@@ -1,37 +1,31 @@
-const { encodeBase64, handleError } = require("./helpers.js");
+const { handleError } = require("./helpers.js");
 const { MIME_FOLDER } = require("./symbols.js");
 
-const DIRECTORY_CREATE_URL = "https://www.googleapis.com/upload/drive/v3/files";
+const DIRECTORY_CREATE_URL = "https://www.googleapis.com/drive/v3/files";
 
 function createDirectory(token, patcher, {
     parentID,
-    // id,
-    // contents = "",
     name
 } = {}) {
     const parents = parentID ? [parentID] : [];
     const options = {
         url: DIRECTORY_CREATE_URL,
         method: "POST",
-        // query: {
-        //     uploadType: "multipart"
-        // },
         headers: {
             Authorization: `Bearer ${token}`,
             "Content-Type": "application/json"
-            // "Content-Length": size.toString()
         },
-        body: JSON.stringify({
+        body: {
             mimeType: MIME_FOLDER,
             name,
             parents
-        })
+        }
     };
     return patcher.execute("request", options)
         .then(res => res.data.id)
         .catch(handleError);
 }
 
-module.export = {
+module.exports = {
     createDirectory
 };
